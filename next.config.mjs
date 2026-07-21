e9a836f45f47711cff49838d8e33b67814eb0950
Jacek Leszczyński <jacek@bobsled.co>
2026-07-21 18:03:25 +0200

    Configure responsive image quality

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    qualities: [75, 90],
     remotePatterns: [
       {
         protocol: 'https',
